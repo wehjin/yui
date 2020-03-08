b808b8cf41af4da91e659299057d9281d1c4a97a
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-03-08 13:59:31 -0700

    Adds support for KEY_RESIZE.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[macro_use]
+extern crate log;
 extern crate ncurses;
+extern crate simplelog;
 
+use std::fs::File;
+
+use log::LevelFilter;
 use ncurses::*;
+use simplelog::{Config, WriteLogger};
 
 use yui::*;
 
@@ -15,28 +22,29 @@ mod ycurses;
 mod yui;
 
 fn main() {
-	let header =
-		label_yard("Buttons", StrokeColor::PrimaryBody, Cling::Custom { x: 0.0, y: 0.0 }).pad(1)
-			.before(fill_yard(FillColor::Primary));
-
-
-	let button_pole =
-		button_yard("Enabled").confine_height(1, Cling::CenterMiddle)
-			.pack_top(1, empty_yard())
-			.pack_top(1, button_yard("Focused"));
-	let content =
-		button_pole.confine(32, 3, Cling::CenterMiddle).pad(1)
-			.before(fill_yard(FillColor::Background));
-	let yard = content.pack_top(3, header);
-
+	WriteLogger::init(LevelFilter::Info, Config::default(), File::create("yui.log").unwrap()).unwrap();
 	initscr();
 	if !has_colors() {
 		endwin();
 		println!("Your terminal does not support color");
 		std::process::exit(1);
 	}
+	let screen = CursesScreen::start(|| {
+		let header =
+			label_yard("Buttons", StrokeColor::PrimaryBody, Cling::Custom { x: 0.0, y: 0.0 }).pad(1)
+				.before(fill_yard(FillColor::Primary));
 
-	CursesScreen::start(yard);
+
+		let button_pole =
+			button_yard("Enabled").confine_height(1, Cling::CenterMiddle)
+				.pack_top(1, empty_yard())
+				.pack_top(1, button_yard("Focused"));
+		let content =
+			button_pole.confine(32, 3, Cling::CenterMiddle).pad(1)
+				.before(fill_yard(FillColor::Background));
+		let yard = content.pack_top(3, header);
+		yard
+	});
 	raw();
 	keypad(stdscr(), true);
 	cbreak();
@@ -46,7 +54,11 @@ fn main() {
 		let ch = getch();
 		match ch {
 			KEY_EOT => {
-				done = true
+				screen.close();
+				done = true;
+			}
+			KEY_RESIZE => {
+				screen.resize_and_refresh();
 			}
 			_ => {
 				println!("KEY: {}", ch);
---
+++ src/ycurses.rs
@@ -0,0 +1,190 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc;
+use std::thread;
+
+use ncurses::*;
+
+use crate::yui::{RenderContext, Yard};
+use crate::yui::bounds::{Bounds, BoundsHold};
+use crate::yui::layout::LayoutContextImpl;
+use crate::yui::palette::{FillColor, Palette, StrokeColor};
+
+pub struct CursesScreen {
+	tx: Sender<ScreenAction>
+}
+
+impl CursesScreen {
+	pub fn start(gen_yard: impl FnOnce() -> Rc<dyn Yard> + std::marker::Send + 'static) -> CursesScreen {
+		let (tx, rx): (Sender<ScreenAction>, Receiver<ScreenAction>) = mpsc::channel();
+		curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+		clear();
+		thread::spawn(move || {
+			let yard = gen_yard();
+			let mut done = false;
+			while !done {
+				let action = rx.recv().unwrap();
+				info!("ACTION: {:?}", action);
+				match action {
+					ScreenAction::ResizeRefresh => {
+						let (max_x, max_y) = Self::size();
+						let (init_index, init_hold) = BoundsHold::init(max_x, max_y);
+						yard.layout(&mut LayoutContextImpl::new(init_index, init_hold.clone()));
+						let palette = Palette::new();
+						let mut ctx = CursesRenderContext::new(
+							max_y,
+							max_x,
+							init_hold.clone(),
+							&palette,
+						);
+						for row in 0..max_y {
+							ctx.row = row;
+							for col in 0..max_x {
+								ctx.col = col;
+								yard.render(&ctx);
+								ctx.publish();
+							}
+						}
+						refresh();
+					}
+					ScreenAction::Close => { done = true }
+				}
+			}
+		});
+		tx.send(ScreenAction::ResizeRefresh).unwrap();
+		CursesScreen { tx }
+	}
+
+	pub fn resize_and_refresh(&self) {
+		self.tx.send(ScreenAction::ResizeRefresh).unwrap();
+	}
+
+	pub fn close(&self) {
+		self.tx.send(ScreenAction::Close).unwrap();
+	}
+
+	fn size() -> (i32, i32) {
+		let mut max_x = 0;
+		let mut max_y = 0;
+		getmaxyx(stdscr(), &mut max_y, &mut max_x);
+		(max_x, max_y)
+	}
+}
+
+
+struct CursesRenderContext<'a> {
+	row: i32,
+	col: i32,
+	bounds_hold: Rc<RefCell<BoundsHold>>,
+	cols: i32,
+	spots: Vec<RefCell<SpotStack<'a>>>,
+}
+
+impl<'a> CursesRenderContext<'a> {
+	fn new(
+		rows: i32,
+		cols: i32,
+		bounds_hold: Rc<RefCell<BoundsHold>>,
+		palette: &'a Palette,
+	) -> Self {
+		let origin_stack = SpotStack::new(palette);
+		CursesRenderContext {
+			row: 0,
+			col: 0,
+			bounds_hold,
+			cols,
+			spots: vec![origin_stack; (rows * cols) as usize].into_iter().map(|it| RefCell::new(it)).collect(),
+		}
+	}
+
+	fn spot_stack(&self) -> &RefCell<SpotStack<'a>> {
+		let index = self.row * self.cols + self.col;
+		&self.spots[index as usize]
+	}
+
+	fn publish(&self) {
+		mv(self.row as i32, self.col as i32);
+		let stack = self.spot_stack();
+		let (color_pair_index, glyph) = stack.borrow().color_pair_index_and_glyph();
+		attrset(COLOR_PAIR(color_pair_index));
+		addch(glyph as chtype);
+	}
+}
+
+impl<'a> RenderContext for CursesRenderContext<'a> {
+	fn spot(&self) -> (i32, i32) {
+		(self.row as i32, self.col as i32)
+	}
+
+	fn yard_bounds(&self, yard_id: i32) -> Bounds {
+		self.bounds_hold.borrow().yard_bounds(yard_id).to_owned()
+	}
+
+	fn set_fill(&self, color: FillColor, z: i32) {
+		self.spot_stack().borrow_mut().set_fill(color, z);
+	}
+
+	fn set_glyph(&self, glyph: char, color: StrokeColor, z: i32) {
+		self.spot_stack().borrow_mut().set_stroke(glyph, color, z);
+	}
+}
+
+
+#[derive(Copy, Clone, Debug)]
+struct SpotStack<'a> {
+	fill_color: FillColor,
+	fill_z: i32,
+	stroke_type: Option<(char, StrokeColor)>,
+	stroke_z: i32,
+	palette: &'a Palette,
+}
+
+impl<'a> SpotStack<'a> {
+	fn new(palette: &'a Palette) -> Self {
+		SpotStack {
+			fill_color: FillColor::Background,
+			fill_z: i32::max_value(),
+			stroke_type: Option::None,
+			stroke_z: i32::max_value(),
+			palette,
+		}
+	}
+
+	fn set_fill(&mut self, color: FillColor, z: i32) {
+		if z <= self.fill_z {
+			self.fill_z = z;
+			self.fill_color = color;
+		}
+	}
+
+	fn set_stroke(&mut self, glyph: char, color: StrokeColor, z: i32) {
+		if z <= self.stroke_z {
+			self.stroke_z = z;
+			self.stroke_type = Option::Some((glyph, color))
+		}
+	}
+
+	fn color_pair_index_and_glyph(&self) -> (i16, char) {
+		let fill_color = self.fill_color;
+		let (glyph, stroke_color) = match self.stroke_type {
+			None => (' ', StrokeColor::Body),
+			Some((glyph, color)) => if self.stroke_z <= self.fill_z {
+				(glyph, color)
+			} else {
+				(' ', StrokeColor::Body)
+			},
+		};
+		let color_pair = self.palette.color_pair_index(stroke_color, fill_color);
+		(color_pair, glyph)
+	}
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+enum ScreenAction {
+	Close,
+	ResizeRefresh,
+}
+
+pub const KEY_EOT: i32 = 4;
+
