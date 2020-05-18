0441e2ec2f792b9a4b6c615ebd98efe9160ce9aa
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-05-17 17:50:24 -0700

    Moves mod quad_label into a file.

---
+++ src/yard/quad_label.rs
@@ -0,0 +1,13 @@
+use crate::{ArcYard, Cling, Pack, yard};
+use crate::yui::palette::{body_and_comment_for_fill, FillColor};
+
+pub fn quad_label(title: &str, subtitle: &str, value: &str, subvalue: &str, value_rows: usize, fill_color: FillColor) -> ArcYard {
+	let (color, subcolor) = body_and_comment_for_fill(fill_color);
+	let title = yard::label(title, color, Cling::LeftTop);
+	let subtitle = yard::label(subtitle, subcolor, Cling::LeftBottom);
+	let value = yard::label(value, color, Cling::RightTop);
+	let subvalue = yard::label(subvalue, subcolor, Cling::RightBottom);
+	let left = title.pack_bottom(1, subtitle);
+	let right = value.pack_bottom(1, subvalue);
+	left.pack_right(value_rows as i32, right)
+}
