450e12814906debd26508c4a2a4cb4f6a7b63d50
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-06-12 07:59:32 -0700

    Updates panic message for StoryScope.

--- src/story/scope.rs
+++ src/story/scope.rs
@@ -29,8 +29,8 @@ impl<V: Clone, A, R> StoryScope<V, A, R> {
 		watcher.send(self.vision.clone()).unwrap();
 	}
 
-	pub fn new(vision: V, link: Link<A>, ctx: Option<Edge>, on_report: impl Fn(R) + Sync + Send + 'static) -> Self {
-		StoryScope { vision, watchers: HashMap::new(), link, on_report: Arc::new(on_report), edge: ctx }
+	pub fn new(vision: V, link: Link<A>, edge: Option<Edge>, on_report: impl Fn(R) + Sync + Send + 'static) -> Self {
+		StoryScope { vision, watchers: HashMap::new(), link, on_report: Arc::new(on_report), edge }
 	}
 }
 
@@ -44,14 +44,14 @@ impl<S, A, R> Flow<S, A, R> for StoryScope<S, A, R> {
 	{
 		let report_link = Link::new(on_report);
 		match &self.edge {
-			None => panic!("No context"),
+			None => panic!("No edge in StoryScope"),
 			Some(ctx) => ctx.start_dialog::<Sprk>(spark, report_link),
 		}
 	}
 
 	fn end_prequel(&self) {
 		match &self.edge {
-			None => panic!("No context"),
+			None => panic!("No edge in StoryScope"),
 			Some(ctx) => ctx.end_dialog(),
 		}
 	}
