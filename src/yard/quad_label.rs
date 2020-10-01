264d745445c7d7cf03f77873e57a9b2ddce0a08d
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-10-01 04:53:58 -0700

    Renames quad_label parameter to value_cols.

--- src/yard/quad_label.rs
+++ src/yard/quad_label.rs
@@ -1,7 +1,7 @@
 use crate::{ArcYard, Cling, Pack, yard};
 use crate::palette::{body_and_comment_for_fill, FillColor};
 
-pub fn quad_label(title: &str, subtitle: &str, value: &str, subvalue: &str, value_rows: usize, fill_color: FillColor) -> ArcYard {
+pub fn quad_label(title: &str, subtitle: &str, value: &str, subvalue: &str, value_cols: usize, fill_color: FillColor) -> ArcYard {
 	let (color, subcolor) = body_and_comment_for_fill(fill_color);
 	let title = yard::label(title, color, Cling::LeftTop);
 	let subtitle = yard::label(subtitle, subcolor, Cling::LeftBottom);
@@ -9,5 +9,5 @@ pub fn quad_label(title: &str, subtitle: &str, value: &str, subvalue: &str, valu
 	let subvalue = yard::label(subvalue, subcolor, Cling::RightBottom);
 	let left = title.pack_bottom(1, subtitle);
 	let right = value.pack_bottom(1, subvalue);
-	left.pack_right(value_rows as i32, right)
+	left.pack_right(value_cols as i32, right)
 }
