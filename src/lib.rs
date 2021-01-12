44a468b26dc1c2ad1151763f9955dd38917b796a
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2021-01-12 12:08:44 -0800

    Update dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,21 @@ pub mod yard;
 mod link;
 mod yui;
 mod yui_curses;
+
+
+mod surface {
+	/**
+	* The surface type
+	*/
+	pub enum Type {
+		Body,
+		Header,
+		Navigation,
+	}
+
+	pub enum Texture {
+		Plain,
+		Focused,
+		Pressed,
+	}
+}
